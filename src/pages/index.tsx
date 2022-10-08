6cf040177acbde26669828eb54627ee8ccd49a6a
Srikar Manikonda <avatsara3@gmail.com>
2022-10-08 15:12:23 -0500

    more

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,24 +1,17 @@
-import EventList from "../components/EventList";
 import React, {useState, useEffect} from "react";
 import MainHeader from "../components/main-header";
 import styles from "../styles/index.module.scss";
-import {X} from "../components/X";
-const Index = () => {
-        const [data, setData] = useState({
-          id:"",
-          name:"",
-          description:""
-
-
-
-        });
+import {IUser} from "../components/Iuser";
+import Table from "../components/EventList";
+import axios from "axios";
+function Index() {
+        const [data, setData] = useState<IUser[]>([]);
       
         const fetchData = async () => {
           const uri = 'https://api.hackillinois.org/event/';
-          const response =  await fetch(uri); 
-          console.log(response);
-          console.loga(response.data);
+          const response = await axios.get<IUser[]>(uri);
           setData(response.data);
+
         };
         //fetch the api here.
         useEffect(() => {
@@ -28,7 +21,7 @@ const Index = () => {
             <>
                 <MainHeader />
                 <p className={styles.myParagraph}>  Welcome to the events page!</p>
-                <EventList data = {"hi"}/> 
+                <Table data = {data}/> 
             </>
             
     
