f509df792472f997a790aa88d4bd2d6f01d9c64a
Srikar Manikonda <avatsara3@gmail.com>
2022-10-08 13:02:23 -0500

    tried

---
+++ src/components/X.ts
@@ -0,0 +1,8 @@
+export interface X {
+    id: number;
+    name: string;
+    description: string;
+
+    
+  }
+  
--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -1,13 +1,42 @@
+import EventList from "../components/EventList";
+import React, {useState, useEffect} from "react";
 import MainHeader from "../components/main-header";
 import styles from "../styles/index.module.scss";
-
+import {X} from "../components/X";
 const Index = () => {
-    return (
-        <>
-            <MainHeader />
-            <p className={styles.myParagraph}>  Welcome to the events page!</p>
-        </>
-    );
-};
+        const [data, setData] = useState({
+          id:"",
+          name:"",
+          description:""
+
+
+
+        });
+      
+        const fetchData = async () => {
+          const uri = 'https://api.hackillinois.org/event/';
+          const response =  await fetch(uri); 
+          console.log(response);
+          console.loga(response.data);
+          setData(response.data);
+        };
+        //fetch the api here.
+        useEffect(() => {
+          fetchData();
+        }, []);
+        return (
+            <>
+                <MainHeader />
+                <p className={styles.myParagraph}>  Welcome to the events page!</p>
+                <EventList data = {"hi"}/> 
+            </>
+            
+    
+        );
+    ;
+}  
+      
+
+   
 
 export default Index;
